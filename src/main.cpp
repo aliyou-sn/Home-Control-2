dd997e086de9266658958ef1dc4d1b5cb9ae183a
Aliyu Nura Salisu <engrsnaliyu@gmail.com>
2023-06-21 14:16:13 +0100

    updating webpage

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,22 @@
 // #include "Lights/light6.h"
 #include "Sirens/Siren1.h"
 #include "weathers/Room1.h"
+
+#define timeSeconds 10
+#define NUM_RELAYS  4
+#define RELAY_NO    false
+
+
 const char* ssid = "Aliyu";
 const char* password = "macbooker";
-#define timeSeconds 10
+const char* PARAM_INPUT_1 = "relay";  
+const char* PARAM_INPUT_2 = "state";
 
 AsyncWebServer server(80);
+
+int relayGPIOs[NUM_RELAYS] = {L1, L2, L3, L4};
+
+
 
 void light1();
 void light2();
@@ -24,7 +35,7 @@ void light4();
 
 
 const char index_html[] PROGMEM = R"rawliteral(
-<!DOCTYPE HTML><html>
+  <!DOCTYPE HTML><html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
@@ -43,6 +54,13 @@ const char index_html[] PROGMEM = R"rawliteral(
       vertical-align:middle;
       padding-bottom: 15px;
     }
+  body {max-width: 600px; margin:0px auto; padding-bottom: 25px;}
+  .switch {position: relative; display: inline-block; width: 120px; height: 68px} 
+  .switch input {display: none}
+  .slider {position: absolute; top: 0; left: 0; right: 0; bottom: 0; background-color: #ccc; border-radius: 34px}
+  .slider:before {position: absolute; content: ""; height: 52px; width: 52px; left: 8px; bottom: 8px; background-color: #fff; -webkit-transition: .4s; transition: .4s; border-radius: 68px}
+  input:checked+.slider {background-color: #2196F3}
+  input:checked+.slider:before {-webkit-transform: translateX(52px); -ms-transform: translateX(52px); transform: translateX(52px)}
   </style>
 </head>
 <body>
@@ -59,6 +77,8 @@ const char index_html[] PROGMEM = R"rawliteral(
     <span id="humidity">%HUMIDITY%</span>
     <sup class="units">&percnt;</sup>
   </p>
+  %BUTTONPLACEHOLDER%
+  
 </body>
 <script>
 setInterval(function ( ) {
@@ -82,9 +102,36 @@ setInterval(function ( ) {
   xhttp.open("GET", "/humidity", true);
   xhttp.send();
 }, 10000 ) ;
-</script>
-</html>)rawliteral";
 
+function toggleCheckbox(element) {
+  var xhr = new XMLHttpRequest();
+  if(element.checked){ xhr.open("GET", "/update?relay="+element.id+"&state=1", true); }
+  else { xhr.open("GET", "/update?relay="+element.id+"&state=0", true); }
+  xhr.send();
+}
+</script>
+</html>
+)rawliteral";
+
+String relayState(int numRelay){
+  if(RELAY_NO){
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "";
+    }
+    else {
+      return "checked";
+    }
+  }
+  else {
+    if(digitalRead(relayGPIOs[numRelay-1])){
+      return "checked";
+    }
+    else {
+      return "";
+    }
+  }
+  return "";
+}
 // Replaces placeholder with DHT values
 String processor(const String& var){
   //Serial.println(var);
@@ -94,8 +141,19 @@ String processor(const String& var){
   else if(var == "HUMIDITY"){
     return String(t1);
   }
+  else if(var == "BUTTONPLACEHOLDER"){
+    String buttons ="";
+    for(int i=1; i<=NUM_RELAYS; i++){
+      String relayStateValue = relayState(i);
+      buttons+= "<h4> Light " + String(i) + "</h4><label class=\"switch\"><input type=\"checkbox\" onchange=\"toggleCheckbox(this)\" id=\"" + String(i) + "\" "+ relayStateValue +"><span class=\"slider\"></span></label>";
+    }
+    return buttons;
+  }
+
   return String();
 }
+
+
 
 
 unsigned long now = millis();
@@ -147,6 +205,33 @@ void setup() {
     request->send_P(200, "text/plain", String(h1).c_str());
   });
 
+  server.on("/update", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    String inputMessage2;
+    String inputParam2;
+    // GET input1 value on <ESP_IP>/update?relay=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1) & request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+      inputMessage2 = request->getParam(PARAM_INPUT_2)->value();
+      inputParam2 = PARAM_INPUT_2;
+      if(RELAY_NO){
+        Serial.print("NO ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], !inputMessage2.toInt());
+      }
+      else{
+        Serial.print("NC ");
+        digitalWrite(relayGPIOs[inputMessage.toInt()-1], inputMessage2.toInt());
+      }
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    // Serial.println(inputMessage + inputMessage2);
+    request->send(200, "text/plain", "OK");
+  });
   // Start server
   server.begin();
 }
